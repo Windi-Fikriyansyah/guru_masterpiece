c3ed09dbf443732f04156b789412ead8b1d528cc
windi fikriyansyah <diwin321@gmail.com>
2026-02-06 17:39:07 +0700

    a

--- resources/views/admin/lkpd/index.blade.php
+++ resources/views/admin/lkpd/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 
-@section('header', 'LKPD Generator')
+@section('header', 'LKPD Siap Pakai')
 
 @push('styles')
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />

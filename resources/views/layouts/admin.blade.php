e58deacd8a3cb21249d751207d2631d5ee4d214f
windi fikriyansyah <diwin321@gmail.com>
2026-02-09 15:14:23 +0700

    a

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} - Admin Dashboard</title>
+    <title>{{ config('app.name', 'guru masterpiece') }} - guru masterpiece</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
